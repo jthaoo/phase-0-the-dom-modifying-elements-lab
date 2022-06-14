1601a87685564cf3034f6d8f987580e76798654a
Johnathan Thao <johnathanthao21@gmail.com>
2022-06-14 10:07:11 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Johnathan is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Attempt
+
+
+// // Write your code here!
+// main.remove()
+// const newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader)
+// newHeader.id = 'victory'
+// document.body.appendChild(newHeader)
+// newHeader.id = 'victory'
+// newHeader.innerHTML = "Matt is the champion"
